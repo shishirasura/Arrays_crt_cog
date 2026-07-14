01237067ad6cb66ae9b535d575f965c4e50fd58e
shishirasura <shishirasura@gmail.com>
2026-07-14 18:00:40 +0530

    Update money_robb.py

--- money_robb.py
+++ money_robb.py
@@ -12,19 +12,3 @@ Output :-
 300
 '''
 //CODE
-def newstring(s):
-    letter=''
-    digit=''
-    if(s[0].isdigit()):
-        digit+=s[0]
-    elif(s[0].isalpha()):
-        letter+=s[0]
-    for ind in range(1,len(s)):
-        if((s[ind-1].isdigit() and s[ind].isalpha()) or (s[ind-1].isalpha() and s[ind].isdigit())):
-            if(s[ind].isdigit()):
-                digit+=s[ind]
-            else:
-                letter+=s[ind]
-    return letter+digit
-s=input()
-print(newstring(s))
